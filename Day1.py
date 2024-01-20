9209d893a0035af14b0a1cf088615a9a231d710f
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-01-20 19:31:49 +0500

    First Day of Python Done

---
+++ Day1.py
@@ -0,0 +1,47 @@
+#pip install pandas/sckit-learning/numpy to install libraries that helps in machine learning
+# print("Hello World")
+
+# #Single Assignment of values to Variables
+# x= 19
+# y = "Rayyan"
+# print(y,x);
+# print("Type of variables")
+# print(type(y),type(x))
+
+# #Multiple Assignments
+# w,x,y,z = "rayyan","sayyam","hasnain","noor"
+
+# siblings = ["rayyan","sayyam","hasnain","noor"]
+# o,t,th,f = siblings
+
+#functions
+# adin = "Rayyan Bey"
+
+# def MyName():
+#     adin = "Rayyan"
+#     print("My Name is " + adin)
+
+# MyName()
+# print("Updated:" + adin)
+    
+# z = 1j
+# print(type(z))
+
+# for x in "Rayyan":
+#   print(x)
+
+# n = "Rayyan will comeback stronger in this Semester  ";
+# print(n[3]) ; print(len(n))
+# print("Rayyan" in n);
+# if "comeback" in n:
+#   print("Suiiiiiiii")
+
+# n.split();
+# n.replace("R","r");
+# n.strip();
+
+# age = 19;
+# adin = "rayyan";
+
+# complete = "My Name is {} and my age is {}";
+# print(complete.format(adin,age));
