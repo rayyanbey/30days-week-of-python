3193ef5ea8d40c5cb2aa60e55315c660db2659eb
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-01-31 19:19:09 +0500

    day 1

---
+++ d1.py
@@ -0,0 +1,31 @@
+#formatted Strings
+# first = "Muhammad Rayyan"
+# last = "Asghar"
+# full_name = f'My name is {first} {last} and i am a student'
+# print(full_name) 
+
+#string methods
+# adin = "Rayyan"
+# print(len(adin))
+# print(adin.upper(),adin.lower())
+# print(adin.find('yan'))
+# print(adin.replace('yan','yam'))
+# print('yan' in adin)
+# print(adin.title())
+
+#arithmetic operations
+# print(10/3,10%3,10**3)
+# x = 10 ; y = 5
+# x+=1;y*=10
+
+#operator precedence
+#parenthesis->exponentiation->multiplication->divison->add->subs
+
+
+#math functions
+# import math
+# x = 2.9
+# print(round(x))
+# print(abs(-8.6))
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
