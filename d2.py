4295d1d6254ced7eaeab68404a76c36889a06aa1
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-02-01 10:45:23 +0500

    Day 2 done

---
+++ d2.py
@@ -0,0 +1,23 @@
+#IF ELSE CONDITIONS
+
+# is_hot = True
+# is_cold = True
+# if  is_hot :
+#     print("It's hot day")
+# elif is_cold:
+#     print("It's a cold day")
+# else:
+#     print("It's a Pleasent day")
+
+# print("Enjoy Your Day")
+
+#Exercise
+# price = 1000000
+# credit = (input("Do you have a Good Credit?"))
+# down_payment = 0 
+# if credit == "Yes":
+#     down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2*price
+
+# print("Your Down Payment is {down_payment}")
