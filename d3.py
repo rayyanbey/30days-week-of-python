c42d8d05186aeea96f50ba913e4d45e1784392aa
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-02-03 17:14:14 +0500

    Updated

--- d3.py
+++ d3.py
@@ -1,27 +1,36 @@
-#Logical Operators 
+# #Logical Operators 
 
 
 
-has_high_income = True
-has_good_credit = False
+# has_high_income = True
+# has_good_credit = False
 
-if has_high_income and has_good_credit:
-    print("Eligible for Loan")
-elif has_high_income or has_good_credit:
-    print("Eligible for Interview")
+# if has_high_income and has_good_credit:
+#     print("Eligible for Loan")
+# elif has_high_income or has_good_credit:
+#     print("Eligible for Interview")
 
 
-#AND : Both conditions should be true
-#OR : One should be true to execute the conditionals 
-#NOT : Inverses the condition e.g not TRUE -> False
+# #AND : Both conditions should be true
+# #OR : One should be true to execute the conditionals 
+# #NOT : Inverses the condition e.g not TRUE -> False
     
-#Comparison Operators
+# #Comparison Operators
 
-temprature = 30
+# temprature = 30
 
-if temprature > 30:
-    print("Hot Day")
-elif temprature == 30:
-    print("Pleasent Day")
+# if temprature > 30:
+#     print("Hot Day")
+# elif temprature == 30:
+#     print("Pleasent Day")
 
-# > , >= , < , <= , ==
+# # > , >= , < , <= , ==
+
+#While loops
+
+# i = 1
+# while i<=5 :
+#     print('*' * i)
+#     i+=1
+
+# print("Done")
---
+++ exercise.py
@@ -0,0 +1,28 @@
+###weight_converter
+
+# option = int(input('''Choose conversion option:
+#                1. (L)bs 2. (K)gs'''))
+# weight = float(input("Enter Your Weight: "))
+# new_weight = 0
+# if option == 1:
+#     new_weight = weight / 2.2
+# elif option == 2:
+#     new_weight = weight*2.2
+# else:
+#     print("INVALID OPTION")
+
+# print("Your converted weight is:", new_weight)
+
+###Guess_Game
+# secret = 10
+
+# i = 1
+# while i<=3:
+#     val = int(input("Guess Number: "))
+#     if val == secret:
+#         print("You Won")
+#         break
+#     elif i == 3:
+#         print("You Failed")
+#     i+=1
+
