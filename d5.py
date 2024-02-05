cca78b8330a772c38c37a47282d48d08f064a591
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-02-05 15:02:56 +0500

    Day 5

---
+++ d5.py
@@ -0,0 +1,11 @@
+# #Try Except
+# try:
+#     age = int(input("Enter Age:"))
+#     income = 200000
+#     risk = income/age
+#     print(age)
+# except ValueError:
+#     print("Invalid Value")
+# except ZeroDivisionError:
+#     print("Age cannot be Zero")
+
