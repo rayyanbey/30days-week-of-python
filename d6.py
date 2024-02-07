951cc602a6491f4ffc01138f16896cdf0474389f
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-02-07 18:45:43 +0500

    Updated with Exercise

---
+++ d6.py
@@ -0,0 +1,29 @@
+#Classes
+
+# class Point:
+#     def __init__(self,x,y):
+#         self.x = x
+#         self.y = y
+#     def move(self):
+#         print("Move")
+#     def draw(self):
+#         print("draw")
+
+
+# point = Point(10,20)
+# print(point.x)
+
+#Exercise
+# class person:
+    
+#     def __init__(self,Name):
+#         self.Name = Name
+#     def talk(self):
+#         print("My Name is" , self.Name)
+    
+
+# p1 = person("Rayyan")
+# p1.Name = "Muhammad Rayyan Asghar"
+
+# p1.talk()
+    
